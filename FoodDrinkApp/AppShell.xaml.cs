9ab2a6e0f33e840eef17e1d1862fb6d92298bc76
Wang789473 <1195108670@qq.com>
2026-05-29 16:58:33 +0800

    init FoodDrinkApp

---
+++ FoodDrinkApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FoodDrinkApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FoodDrinkApp/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace FoodDrinkApp;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnRecommendClicked(object sender, EventArgs e)
+    {
+        StatusLabel.Text = "Today's Recommendation: 🍜 Beef Noodles | 🍛 Chicken Curry | 🥗 Garden Salad";
+    }
+
+    private void OnCameraClicked(object sender, EventArgs e)
+    {
+        StatusLabel.Text = "Camera ready. Photo feature coming soon.";
+    }
+
+    private void OnSpeakClicked(object sender, EventArgs e)
+    {
+        StatusLabel.Text = "Voice assistant ready. TTS feature coming soon.";
+    }
+}
